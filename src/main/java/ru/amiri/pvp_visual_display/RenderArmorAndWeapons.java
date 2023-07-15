d3bd0d3cfba8a1de41a63258cb9b2e941b0fceb7
Amiri <iuak228@gmail.com>
2023-07-15 21:03:59 +0300

    v17(update render armor)

--- src/main/java/ru/amiri/pvp_visual_display/RenderArmorAndWeapons.java
+++ src/main/java/ru/amiri/pvp_visual_display/RenderArmorAndWeapons.java
@@ -17,31 +17,40 @@ import net.minecraftforge.fml.common.Mod;
 public class RenderArmorAndWeapons {
     @SubscribeEvent
     public static void onPostRenderGameOverlay(RenderGameOverlayEvent.Post event) {
-        if (event.getType() == RenderGameOverlayEvent.ElementType.ALL){
-        Minecraft mc = Minecraft.getInstance();
-        PlayerEntity playerEntity = mc.player;
-        FontRenderer fontRenderer = mc.font;
-        int x = 50;
-        int y = 30;
-         event.getMatrixStack().pushPose();
-        for (int i = 0; i < playerEntity.inventory.armor.size(); i++) {
-            ItemStack stack = playerEntity.inventory.armor.get(i);
-            if (!stack.isEmpty() && (stack.getItem() instanceof ArmorItem || stack.getItem() instanceof ElytraItem)) {
-                int maxDamage = stack.getMaxDamage();
-                int currentDamage = maxDamage - stack.getDamageValue();
-                int percent = (int) ((float) currentDamage / (float) maxDamage * 100);
-                String strengthAndPercent = currentDamage + " (" + percent + "%)";
+        if (event.getType() == RenderGameOverlayEvent.ElementType.ALL) {
+            Minecraft mc = Minecraft.getInstance();
+            PlayerEntity playerEntity = mc.player;
+            FontRenderer fontRenderer = mc.font;
 
-                mc.getItemRenderer().renderAndDecorateItem(stack, x - 20, y - 10);
+            int screenWidth = event.getWindow().getGuiScaledWidth();
+            int screenHeight = event.getWindow().getGuiScaledHeight();
+            int textureWidth = 16; // Ширина текстуры
+            int textureHeight = 16; // Высота текстуры
+            int barHeight = 3; // Высота полоски прочности
 
-                RenderHelper.setupForFlatItems();
-                mc.getItemRenderer().renderGuiItemDecorations(mc.font, stack, x - 20, y - 10);
+            int x = screenWidth - textureWidth; // X-координата текстуры
+            int y = screenHeight - (playerEntity.inventory.armor.size() * (textureHeight + barHeight)); // Y-координата текстуры
 
-                fontRenderer.drawShadow(event.getMatrixStack(), strengthAndPercent, x, y, 0x40cfff);
+            event.getMatrixStack().pushPose();
+            for (int i = playerEntity.inventory.armor.size() - 1; i >= 0; i--) {
+                ItemStack stack = playerEntity.inventory.armor.get(i);
+                if (!stack.isEmpty() && (stack.getItem() instanceof ArmorItem || stack.getItem() instanceof ElytraItem)) {
+                    int maxDamage = stack.getMaxDamage();
+                    int currentDamage = maxDamage - stack.getDamageValue();
+                    int percent = (int) ((float) currentDamage / (float) maxDamage * 100);
+                    String strengthAndPercent = currentDamage + " (" + percent + "%)";
+
+                    mc.getItemRenderer().renderAndDecorateItem(stack, x, y); // Текстура
+
+                    RenderHelper.setupForFlatItems();
+                    mc.getItemRenderer().renderGuiItemDecorations(mc.font, stack, x, y); // Полоска
+
+                    fontRenderer.drawShadow(event.getMatrixStack(), strengthAndPercent, x, y + textureHeight, 0x40cfff); // Текст
+
+                    y += textureHeight + barHeight; // Увеличиваем Y-координату для следующего элемента
+                }
             }
-            y -= 15;
-        }
-         event.getMatrixStack().popPose();
+            event.getMatrixStack().popPose();
         }
     }
 }
