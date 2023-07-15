9501d85ec9520912553c7b2a76da892ba10d7c96
Amiri <iuak228@gmail.com>
2023-07-15 14:02:11 +0300

    v16(update render armor)

--- src/main/java/ru/amiri/pvp_visual_display/RenderArmorAndWeapons.java
+++ src/main/java/ru/amiri/pvp_visual_display/RenderArmorAndWeapons.java
@@ -3,10 +3,10 @@ package ru.amiri.pvp_visual_display;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ElytraItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -17,11 +17,12 @@ import net.minecraftforge.fml.common.Mod;
 public class RenderArmorAndWeapons {
     @SubscribeEvent
     public static void onPostRenderGameOverlay(RenderGameOverlayEvent.Post event) {
+        if (event.getType() == RenderGameOverlayEvent.ElementType.ALL){
         Minecraft mc = Minecraft.getInstance();
         PlayerEntity playerEntity = mc.player;
         FontRenderer fontRenderer = mc.font;
         int x = 50;
-        int y = 50;
+        int y = 30;
          event.getMatrixStack().pushPose();
         for (int i = 0; i < playerEntity.inventory.armor.size(); i++) {
             ItemStack stack = playerEntity.inventory.armor.get(i);
@@ -31,10 +32,16 @@ public class RenderArmorAndWeapons {
                 int percent = (int) ((float) currentDamage / (float) maxDamage * 100);
                 String strengthAndPercent = currentDamage + " (" + percent + "%)";
 
+                mc.getItemRenderer().renderAndDecorateItem(stack, x - 20, y - 10);
+
+                RenderHelper.setupForFlatItems();
+                mc.getItemRenderer().renderGuiItemDecorations(mc.font, stack, x - 20, y - 10);
+
                 fontRenderer.drawShadow(event.getMatrixStack(), strengthAndPercent, x, y, 0x40cfff);
             }
-            y -= 10;
+            y -= 15;
         }
          event.getMatrixStack().popPose();
+        }
     }
 }
