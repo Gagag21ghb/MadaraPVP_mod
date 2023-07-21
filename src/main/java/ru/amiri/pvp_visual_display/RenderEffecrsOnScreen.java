554152943d12a575f706179cdcf59ccc2161e341
Amiri <iuak228@gmail.com>
2023-07-22 00:10:22 +0300

    v22(stable version)

--- src/main/java/ru/amiri/pvp_visual_display/RenderEffecrsOnScreen.java
+++ src/main/java/ru/amiri/pvp_visual_display/RenderEffecrsOnScreen.java
@@ -17,49 +17,46 @@ import java.util.Collection;
 
 @Mod.EventBusSubscriber
 public class RenderEffecrsOnScreen {
-
     @SubscribeEvent
-    public static void onPostRenderGameOverlay(RenderGameOverlayEvent.Post event) {
-        if (event.getType() != RenderGameOverlayEvent.ElementType.POTION_ICONS)
-            return;
-        Minecraft mc = Minecraft.getInstance();
-        Collection<EffectInstance> effects = mc.player.getActiveEffects();
-        FontRenderer fontRenderer = mc.font;
+    public static void onPostRenderGameOverlay(RenderGameOverlayEvent.Pre event) {
+        if (event.getType() == RenderGameOverlayEvent.ElementType.POTION_ICONS) {
+            event.setCanceled(true);
+        }
+        if(event.getType() == RenderGameOverlayEvent.ElementType.HOTBAR){
+            Minecraft mc = Minecraft.getInstance();
+            assert mc.player != null;
+            Collection<EffectInstance> effects = mc.player.getActiveEffects();
+            FontRenderer fontRenderer = mc.font;
 
+            int x = 100;
+            int y = 100;
+            int yOffset = 20;
 
-        int x = 100;
-        int y = 100;
-        int yOffset = 20;
+            for (EffectInstance effectInstance : effects) {
+                Effect effect = effectInstance.getEffect();
+                int duration = effectInstance.getDuration();
+                effectInstance.getCurativeItems();
 
-        for (EffectInstance effectInstance : effects) {
-            Effect effect = effectInstance.getEffect();
-            int duration = effectInstance.getDuration();
-            effectInstance.getCurativeItems();
+                PotionSpriteUploader potionspriteuploader = mc.getMobEffectTextures();
+                TextureAtlasSprite sprite = potionspriteuploader.get(effect);
+                mc.getTextureManager().bind(sprite.atlas().location());
 
-            PotionSpriteUploader potionspriteuploader = mc.getMobEffectTextures();
-            TextureAtlasSprite sprite = potionspriteuploader.get(effect);
-            mc.getTextureManager().bind(sprite.atlas().location());
-            int k = event.getWindow().getGuiScaledWidth();
-            int k1 = 1;
+                String timeRemaining = getTimeRemaining(duration);
+                int textWidth = fontRenderer.width(timeRemaining);
+                int textX = x + (sprite.getWidth() - textWidth) / 2;
+                int textY = y + sprite.getHeight() + 2;
+                AbstractGui.blit(event.getMatrixStack(), textX, textY, 0, 18, 18, sprite);
+                fontRenderer.drawShadow(event.getMatrixStack(), timeRemaining, textX, textY, effect.getColor());
 
-
-
-            String timeRemaining = getTimeRemaining(duration);
-            int textWidth = fontRenderer.width(timeRemaining);
-            int textX = x + (sprite.getWidth() - textWidth) / 2;
-            int textY = y + sprite.getHeight() + 2;
-            AbstractGui.blit(event.getMatrixStack(), textX, textY, 0, 18, 18, sprite);
-            fontRenderer.drawShadow(event.getMatrixStack(), timeRemaining, textX, textY, effect.getColor());
-
-            y += yOffset;
+                y += yOffset;
+            }
         }
     }
-    private static String getTimeRemaining(int duration) {
-        int seconds = duration / 20;
-        int minutes = seconds / 60;
-        seconds %= 60;
+        private static String getTimeRemaining (int duration){
+            int seconds = duration / 20;
+            int minutes = seconds / 60;
+            seconds %= 60;
 
-        return String.format("%02d:%02d", minutes, seconds);
+            return String.format("%02d:%02d", minutes, seconds);
+        }
     }
-
-}
