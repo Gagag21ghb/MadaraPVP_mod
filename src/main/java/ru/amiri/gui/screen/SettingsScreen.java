21839cbdf3d9fa426db8c4e17938644c9c4e0a39
Amiri <iuak228@gmail.com>
2023-07-30 20:43:24 +0300

    v2.4(update settings)

--- src/main/java/ru/amiri/gui/screen/SettingsScreen.java
+++ src/main/java/ru/amiri/gui/screen/SettingsScreen.java
@@ -2,14 +2,11 @@ package ru.amiri.gui.screen;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.Style;
-import net.minecraft.util.text.event.ClickEvent;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -21,7 +18,7 @@ public class SettingsScreen extends Screen {
     private static final ResourceLocation LOLOLOSHKA = new ResourceLocation(Madara.MOD_ID, "textures/gui/lololoshka.jpg");
     private static final ITextComponent clickableText = new StringTextComponent("test").withStyle();
 
-    protected SettingsScreen(ITextComponent p_i51108_1_) {
+    public SettingsScreen(ITextComponent p_i51108_1_) {
           super(p_i51108_1_);
 
         }
@@ -49,21 +46,23 @@ public class SettingsScreen extends Screen {
         int x = (this.width - buttonWidth) / 2;
         int y = this.height / 2 + 40;
 
-        Button myButton = new Button(x, y, buttonWidth, buttonHeight, clickableText, button -> {
+        Button myButton = new ButtonCustom(x, y, buttonWidth, buttonHeight, clickableText, button -> {
 
         });
 
         this.addButton(myButton);
     }
 
-    @SubscribeEvent
-    public static void onKeyInput(InputEvent.KeyInputEvent event) {
-        int keyCode = event.getKey();
+    class ButtonCustom extends Button{
 
-        if (keyCode == GLFW.GLFW_KEY_T) {
-
-             Minecraft.getInstance().setScreen(new SettingsScreen(new StringTextComponent("Another Screen")));
+        public ButtonCustom(int p_i232255_1_, int p_i232255_2_, int p_i232255_3_, int p_i232255_4_, ITextComponent p_i232255_5_, IPressable p_i232255_6_) {
+            super(p_i232255_1_, p_i232255_2_, p_i232255_3_, p_i232255_4_, p_i232255_5_, p_i232255_6_);
         }
 
+        @Override
+        public void renderButton(MatrixStack p_230431_1_, int mouseX, int mouseY, float partialTicks) {
+            super.renderButton(p_230431_1_, mouseX, mouseY, partialTicks); // Рендерим только текст
+
+        }
     }
 }
--- src/main/java/ru/amiri/settings_control/AddButtonSettings.java
+++ src/main/java/ru/amiri/settings_control/AddButtonSettings.java
@@ -1,27 +1,35 @@
 package ru.amiri.settings_control;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.lwjgl.glfw.GLFW;
+import ru.amiri.Madara;
+import ru.amiri.gui.screen.SettingsScreen;
 
-@Mod.EventBusSubscriber
+@Mod.EventBusSubscriber(modid = Madara.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class AddButtonSettings {
     private static final String CATEGORY = "Madara";
-    public static final KeyBinding
-            MY_KEY_FIRST = new KeyBinding("key.open.screen", GLFW.GLFW_KEY_G, CATEGORY);
-
-    public static void register() {
-        setRegister(MY_KEY_FIRST);
+    public static final KeyBinding MY_KEY_FIRST = new KeyBinding("key.open.screen", GLFW.GLFW_KEY_G, CATEGORY);
+    public static SettingsScreen screen;
+    @SubscribeEvent
+    public static void setRegister(FMLClientSetupEvent event) {
+        ClientRegistry.registerKeyBinding(MY_KEY_FIRST);
     }
     @SubscribeEvent
-    public static void onClientSetup(FMLClientSetupEvent  event) {
-        AddButtonSettings.register();
-    }
-
-    private static void setRegister(KeyBinding binding) {
-        ClientRegistry.registerKeyBinding(binding);
+    public static void onKeyInput(InputEvent.KeyInputEvent event) {
+        if (MY_KEY_FIRST.isDown()) {
+            if (screen == null) {
+                screen = new SettingsScreen(new StringTextComponent("text"));
+                Minecraft.getInstance().setScreen(screen);
+            }
+        } else {
+            screen = null;
+        }
     }
 }
